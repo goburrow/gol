ae46cf783cb32ae708016f38b6cd898c10ceb902
Quoc-Viet Nguyen <afelion@gmail.com>
2015-03-08 23:52:43 +1000

    Use channel for async appender

--- async/async.go
+++ async/async.go
@@ -5,43 +5,107 @@ package async
 
 import (
 	"sync"
+	"time"
 
 	"github.com/goburrow/gol"
 )
 
 // Appender sends the logging event to all appenders asynchronously.
 type Appender struct {
-	appenders []gol.Appender
+	// DrainTimeout is maximum duration before timing out flush a channel.
+	DrainTimeout time.Duration
+
 	wg        sync.WaitGroup
+	appenders []gol.Appender
+	chans     []chan *gol.LoggingEvent
+
+	mu     sync.Mutex
+	finish chan struct{}
 }
 
 var _ (gol.Appender) = (*Appender)(nil)
 
-// NewAppender allocates and returns a new Appender
-func NewAppender(appender ...gol.Appender) *Appender {
-	return &Appender{appenders: appender}
+// NewAppender allocates and returns a new Appender.
+func NewAppender(bufferSize int, appenders ...gol.Appender) *Appender {
+	a := &Appender{
+		appenders: appenders,
+		chans:     make([]chan *gol.LoggingEvent, len(appenders)),
+
+		DrainTimeout: 10 * time.Second,
+	}
+	for i, _ := range appenders {
+		a.chans[i] = make(chan *gol.LoggingEvent, bufferSize)
+	}
+	return a
 }
 
-// Append calls all appenders
+// Append sends the event to all appenders.
 func (a *Appender) Append(e *gol.LoggingEvent) {
-	for _, appender := range a.appenders {
-		a.wg.Add(1)
-		go a.appendTo(e, appender)
+	for _, c := range a.chans {
+		select {
+		case c <- e:
+			// Succeed.
+		default:
+			// TODO: retry
+		}
 	}
 }
 
 // Start does not do anything at the moment.
 func (a *Appender) Start() error {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if a.finish != nil {
+		return nil // started
+	}
+	a.finish = make(chan struct{})
+	a.wg.Add(len(a.chans))
+	for i, c := range a.chans {
+		go a.receive(c, a.appenders[i])
+	}
 	return nil
 }
 
 // Stop makes sure all appenders finish. It must be called when exiting.
 func (a *Appender) Stop() error {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if a.finish == nil {
+		return nil // not started
+	}
+	close(a.finish)
 	a.wg.Wait()
+	a.finish = nil
 	return nil
 }
 
-func (a *Appender) appendTo(e *gol.LoggingEvent, appender gol.Appender) {
+func (a *Appender) receive(c chan *gol.LoggingEvent, appender gol.Appender) {
 	defer a.wg.Done()
-	appender.Append(e)
+
+	for {
+		select {
+		case <-a.finish:
+			a.flush(c, appender)
+			return
+		case e := <-c:
+			appender.Append(e)
+		}
+	}
+}
+
+func (a *Appender) flush(c chan *gol.LoggingEvent, appender gol.Appender) {
+	timeout := time.After(a.DrainTimeout)
+	for {
+		select {
+		case <-timeout:
+			return
+		case e := <-c:
+			appender.Append(e)
+		default:
+			// Channel is empty.
+			return
+		}
+	}
 }
--- async/async_test.go
+++ async/async_test.go
@@ -20,7 +20,12 @@ func (c channelWriter) Write(b []byte) (int, error) {
 func TestAppender(t *testing.T) {
 	c := make(chan string)
 
-	appender := NewAppender(gol.NewAppender(channelWriter(c)))
+	appender := NewAppender(1, gol.NewAppender(channelWriter(c)))
+	err := appender.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer appender.Stop()
 	event := &gol.LoggingEvent{
 		FormattedMessage: "run",
 		Name:             "async",
@@ -43,12 +48,15 @@ func TestAppenderStop(t *testing.T) {
 		bytes.Buffer{},
 	}
 
-	appender := NewAppender(
+	appender := NewAppender(1,
 		gol.NewAppender(&buffers[0]),
 		gol.NewAppender(&buffers[1]),
 		gol.NewAppender(&buffers[2]),
 	)
-	appender.Start()
+	err := appender.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
 	event := &gol.LoggingEvent{
 		FormattedMessage: "run",
 		Name:             "async",
@@ -56,10 +64,49 @@ func TestAppenderStop(t *testing.T) {
 		Time:             time.Now(),
 	}
 	appender.Append(event)
-	appender.Stop()
+	err = appender.Stop()
+	if err != nil {
+		t.Fatal(err)
+	}
 	for i, _ := range buffers {
 		if !strings.Contains(buffers[i].String(), "async: run") {
 			t.Fatalf("unexpected message: %#v", buffers[i].String())
 		}
+	}
+}
+
+func TestAppenderLifeCycle(t *testing.T) {
+	var buf bytes.Buffer
+	size := 5
+	appender := NewAppender(size, gol.NewAppender(&buf))
+
+	event := &gol.LoggingEvent{
+		FormattedMessage: "run",
+		Name:             "async",
+		Level:            gol.LevelInfo,
+		Time:             time.Now(),
+	}
+
+	for i := 0; i < size; i++ {
+		appender.Append(event)
+	}
+	err := appender.Stop()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = appender.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = appender.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = appender.Stop()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if strings.Count(buf.String(), "async: run") != size {
+		t.Fatalf("unexpected message: %v", buf.String())
 	}
 }
